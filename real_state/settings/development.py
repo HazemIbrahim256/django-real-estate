1b0933c6c959a4695a99095a8a7443f4bbc89807
hazem ibrahim <hazibra2001@gmaill.com>
2022-12-07 14:51:08 +0200

    change configure django logging

---
+++ real_state/settings/development.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASE = {
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
+    }
+}
